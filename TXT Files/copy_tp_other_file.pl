85a5d0a2cde0eef07bd888f35d964be872c2dc58
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2022-03-05 21:23:54 +0100

    Create copy_tp_other_file.pl

---
+++ TXT Files/copy_tp_other_file.pl
@@ -0,0 +1,18 @@
+use feature 'say';
+
+my $filein = 'file.txt';
+my $fileout = 'fileout.txt';
+
+
+open(my $fh1, '<', $filein) or die "Cannot open file: $!";
+open(my $fh2, '>', $fileout) or die "Cannot open file: $!";
+
+
+while (my $line = <$fh1>){#
+	print $fh2 $line;
+};
+
+close $fh1;
+close $fh2;
+
+say "Done";
