e29ffc2b06531e7758780f82eb6a477cb58d5a1f
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2022-03-05 21:13:29 +0100

    Create read_lines.pl

---
+++ TXT Files/read_lines.pl
@@ -0,0 +1,11 @@
+
+use feature 'say';
+my $filename = 'linesfile.txt';
+
+open(my $fh, '<', $filename) or die "Cannot open file: $!";
+
+while (my $line = <$fh>){#
+	chomp $line;
+	say $line;
+};
+close $fh;
