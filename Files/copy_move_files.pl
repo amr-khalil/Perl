b70246dc656f230e5667a56d00676b6ae6162b8d
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2022-03-07 21:41:59 +0100

    Update copy_move_files.pl

--- Files/copy_move_files.pl
+++ Files/copy_move_files.pl
@@ -6,4 +6,15 @@ use File::Copy;
 my $file_from = "file.txt";
 my $file_to = "../new_file.txt";
 
-copy($file_from, $file_to) or die("Error: Copy failed! Check the file path");
+copy_files($file_from, $file_to);
+
+sub copy_files{
+	# Parameters
+	#$_[0]: file path source
+	#$_[1]: file path distenation
+
+	$file_from = $_[0];
+	$file_to = $_[1];
+	copy($file_from, $file_to) or die("Error: Copy failed! Check the file path");
+	print("Success: copy is finished\n")
+}
